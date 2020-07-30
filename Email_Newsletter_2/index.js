4a2f87ef40d2285379fb42349d135d9af7e57e0c
Ikram Yunizar <ikramyunizar@gmail.com>
2020-07-30 13:39:03 +0700

    styling

--- Email_Newsletter_2/index.js
+++ Email_Newsletter_2/index.js
@@ -11,12 +11,51 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/', (req, res) => {
-    let firstname = req.body.firstname;
-    let lastname = req.boddy.lastname;
-    let email = req.body.email;
+//Key API
+//73fe28a010c0236aa550ea274a1fb761-us17
 
-    console.log(firstname + lastname + email);
+
+//List ID
+//8b604b373d
+
+app.post('/', (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    console.log(firstname);
+
+    const data = {
+        members : [{
+            email_address : email,
+            status : "subscribed",
+            merge_fields : {
+                FNAME : firstname,
+                LNAME : lastname
+            }
+        }
+        ]
+    }
+
+    const jsondata = JSON.stringify(data);
+    const url = "https://us17.api.mailchimp.com/3.0/lists/8b604b373d"
+    const options = {
+        method : "POST",
+        auth: "nizaroo:73fe28a010c0236aa550ea274a1fb761-us17"
+    }
+    const request = https.request(url, options,(response) =>{
+
+        if (response.statusCode == "200"  ){
+            res.sendFile(__dirname + "/success.html")
+        }
+        else {
+            res.sendFile(__dirname + "/failed.html")
+        }
+        response.on("data", (data) => {
+            console.log(JSON.parse(data))
+        })
+    } )
+    request.write(jsondata)
+    request.end();
 })
 
 
